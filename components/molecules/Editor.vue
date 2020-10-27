e3533d0e62605d2b38926c5fac897faa051ea5da
mickey <lemanhhungb01501@gmail.com>
2020-10-28 00:31:04 +0700

    add Editor component

---
+++ components/molecules/Editor.vue
@@ -0,0 +1,57 @@
+<template>
+  <textarea
+    class="component-molecules-editor"
+    @input="handleChange"
+    @keydown="handleCursorPosition"
+    @click="handleCursorPosition"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Editor',
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    onChange: {
+      type: Function,
+      required: true
+    },
+    onCursorPositionChange: {
+      type: Function,
+      required: true
+    }
+  },
+  data () {
+    return {}
+  },
+  mounted () {
+    console.log(this)
+  },
+  methods: {
+    handleCursorPosition (e) {
+      this.onCursorPositionChange(e.target.selectionEnd)
+    },
+    handleChange (e) {
+      this.onChange(e.target.value)
+      this.onCursorPositionChange(e.target.selectionStart)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .component-molecules-editor {
+    background-color: rgba(0, 0, 0, 0.2);
+    border: none;
+    resize: none;
+    outline: none;
+    width: 100%;
+    height: 100%;
+    padding: 15px;
+    color: var(--color);
+    overflow-y: scroll;
+  }
+</style>
