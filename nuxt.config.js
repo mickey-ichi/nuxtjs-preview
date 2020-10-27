d3969d6619156d1651e729e4cbd08671b54f02da
mickey <lemanhhungb01501@gmail.com>
2020-10-27 22:25:13 +0700

    implement toggle button

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,10 +14,15 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    '@/assets/main.css'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    {
+      src: '@/plugins/toggle-button.js',
+      mode: 'client'
+    }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="container">
     <Markdown :source="'# h1 Heading 8-)'" />
+    <ToggleButton :value="darkMode" color="var(--color-28c56e)" @change="handleToggle" />
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    console.log(this.$colorMode)
+    return {
+      darkMode: true
+    }
+  },
+  methods: {
+    handleToggle () {
+      this.darkMode = !this.darkMode
+    }
+  },
   head: {
     title: '記事作成 | SHIMBA'
   }
@@ -16,6 +28,8 @@ export default {
 .container {
   min-height: calc(100vh - 51px);
   display: flex;
+  padding-left: 15px;
+  padding-right: 15px;
 }
 
 </style>
